0e5e79a9a959331dda44d768438f53e649d32f51
Gary Thomas <gary.thomas@appfolio.com>
2016-06-22 13:44:12 -0700

    Remove duplicate events, compare by ID

--- src/reducers/events_manager.js
+++ src/reducers/events_manager.js
@@ -1,5 +1,6 @@
 import { eventsManagerInit } from '../store/default_initial_state';
 import { INIT_EVENTS } from '../actions/actions_types';
+import _ from 'lodash';
 
 const eventsManager = (state = eventsManagerInit, action) => {
   const { type, events } = action;
@@ -8,7 +9,8 @@ const eventsManager = (state = eventsManagerInit, action) => {
 
   switch (type) {
     case INIT_EVENTS:
-      const newEvents = state.events.concat(events);
+      // Combine new events with original, remove duplicates:
+      const newEvents = _.uniqBy(state.events.concat(events), event => event.id);
       newState = Object.assign({}, state, { events: newEvents });
       break;
     default:
