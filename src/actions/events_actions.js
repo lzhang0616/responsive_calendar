85bfc478107c351a6a14c22f7e7777a777bd8583
Li Zhang <ycy828f@gmail.com>
2016-06-23 12:03:17 -0700

    lz - Add data fetch cache

--- src/actions/events_actions.js
+++ src/actions/events_actions.js
@@ -2,7 +2,7 @@ import fetch from 'isomorphic-fetch';
 import { INIT_EVENTS, UPDATE_EVENTS } from './actions_types';
 import { getRange, flattenState } from '../utilities/calendar_helpers';
 
-const updateEvents = (events, init, firstFetch) => {
+const updateEvents = (init, payload) => {
   let type;
   if (init) {
     type = INIT_EVENTS;
@@ -10,40 +10,85 @@ const updateEvents = (events, init, firstFetch) => {
     type = UPDATE_EVENTS;
   }
 
-  return {
-    type,
-    events,
-    firstFetch
-  };
+  return { ...payload, type };
 };
 
-const sourceUrl = (source, state) => {
+const getNewRange = (start, end, cachedStart, cachedEnd, init) => {
+  let newStart = cachedStart.clone();
+  let newEnd = cachedEnd.clone();
+
+  if (init) return [ cachedStart, cachedEnd ];
+
+  if (start.isSameOrAfter(cachedStart) && end.isSameOrBefore(cachedEnd)) return [];
+
+  if (start.isBefore(cachedStart)) {
+    newStart = newStart
+      .add(-2, 'month')
+      .startOf('month')
+      .startOf('week');
+  }
+
+  if (end.isAfter(cachedEnd)) {
+    newEnd = newEnd
+      .add(2, 'month')
+      .endOf('month')
+      .add(1, 'week')
+      .endOf('week');
+  }
+
+  return [ newStart, newEnd ];
+};
+
+const sourceUrl = (source, state, newStart, newEnd, init) => {
   const flatState = flattenState(state);
-  const { startQueryParam, endQueryParam, dateFormatter, date, view } = flatState;
+  const { startQueryParam, endQueryParam, dateFormatter,
+          date, view, cachedStart, cachedEnd } = flatState;
 
-  const [ start, end ] = getRange(date, view).map(day => day.format(dateFormatter));
+  let start = newStart;
+  let end = newEnd;
 
-  const url = `${source}?${startQueryParam}=${start}&${endQueryParam}=${end}`;
+  if (init) {
+    start = cachedStart.clone();
+    end = cachedEnd.clone();
+  } else {
+    if (newStart.isBefore(cachedStart)) end = cachedStart.clone().add(-1, 'day');
+    if (newEnd.isAfter(cachedEnd)) start= cachedEnd.clone().add(1, 'day');
+  }
+
+  const [ startDate, endDate ] = [ start, end ].map(day => day.format(dateFormatter));
+
+  const url = `${source}?${startQueryParam}=${startDate}&${endQueryParam}=${endDate}`;
 
   return encodeURI(url);
 };
 
-export const fetchEvent = (source, init, firstFetch) => {
+export const fetchEvent = (source, init, payload) => {
+  const { newStart, newEnd } = payload;
+
   return (dispatch, getState) => {
-    return fetch(sourceUrl(source, getState()))
+    return fetch(sourceUrl(source, getState(), newStart, newEnd, init))
       .then(response => response.json())
-      .then(events => dispatch(updateEvents(events, init, firstFetch)))
+      .then(events => dispatch(updateEvents(init, { ...payload, events })))
       .catch(err => console.error(err));
   };
 };
 
 export const fetchEventSources = (init = false, index = 0) => {
   return (dispatch, getState) => {
-    const { eventsManager } = getState();
-    const { eventSources } = eventsManager;
+    const { eventSources, cachedStart, cachedEnd, date, view } = flattenState(getState());
+    const [ start, end ] = getRange(date, view);
+    const [ newStart, newEnd ] = getNewRange(start, end, cachedStart, cachedEnd, init);
+    let len = eventSources.length;
 
-    if (index < eventSources.length) {
-      dispatch(fetchEvent(eventSources[index], init, index === 0));
+    const payload = {
+      firstFetch: index === 0,
+      lastFetch: index === len - 1,
+      newStart,
+      newEnd
+    };
+
+    if (index < len && newStart) {
+      dispatch(fetchEvent(eventSources[index], init, payload));
 
       return dispatch(fetchEventSources(init, index + 1));
     } else {
--- src/store/default_initial_state.js
+++ src/store/default_initial_state.js
@@ -1,6 +1,18 @@
 import { today } from '../utilities/calendar_helpers';
 
 const date = today();
+const cachedStart = date
+        .clone()
+        .add(-1, 'month')
+        .startOf('month')
+        .startOf('week');
+
+const cachedEnd = date
+        .clone()
+        .add(1, 'month')
+        .endOf('month')
+        .add(1, 'week')
+        .endOf('week');
 
 export const calendarManagerInit = {
   selected: date,
@@ -13,7 +25,9 @@ export const eventsManagerInit = {
   eventSources: ['/calendar/events'],
   startQueryParam: 'start',
   endQueryParam: 'end',
-  dateFormatter: 'YYYY-MM-DD'
+  dateFormatter: 'YYYY-MM-DD',
+  cachedStart,
+  cachedEnd
 };
 
 export default {
