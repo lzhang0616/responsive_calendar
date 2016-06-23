2f9ffd5b7c45c4a01c3354afcb0ec06e8252df5e
Li Zhang <ycy828f@gmail.com>
2016-06-22 22:10:21 -0700

    lz - Fetch upon view change

--- src/actions/events_actions.js
+++ src/actions/events_actions.js
@@ -1,31 +1,53 @@
-import { INIT_EVENTS } from './actions_types';
+import { INIT_EVENTS, UPDATE_EVENTS } from './actions_types';
 import fetch from 'isomorphic-fetch';
+import { flattenState } from '../utilities/store_helpers';
+import { getStartDate, getEndDate } from '../utilities/calendar_helpers';
 
-const initEvents = events => {
+const updateEvents = (events, init, firstFetch) => {
+  let type;
+  if (init) {
+    type = INIT_EVENTS;
+  } else {
+    type = UPDATE_EVENTS;
+  }
+
   return {
-    type: INIT_EVENTS,
-    events
+    type,
+    events,
+    firstFetch
   };
 };
 
-export const fetchEvent = source => {
-  return dispatch => {
-    return fetch(source)
+const sourceUrl = (source, state) => {
+  const flatState = flattenState(state);
+  const { startQueryParam, endQueryParam, dateFormatter, date, view } = flatState;
+
+  const start = getStartDate(date, view).format(dateFormatter);
+  const end = getEndDate(date, view).format(dateFormatter);
+
+  const url = `${source}?${startQueryParam}=${start}&${endQueryParam}=${end}`;
+
+  return encodeURI(url);
+};
+
+export const fetchEvent = (source, init, firstFetch) => {
+  return (dispatch, getState) => {
+    return fetch(sourceUrl(source, getState()))
       .then(response => response.json())
-      .then(events => dispatch(initEvents(events)))
+      .then(events => dispatch(updateEvents(events, init, firstFetch)))
       .catch(err => console.error(err));
   };
 };
 
-export const fetchEventSources = (index = 0) => {
+export const fetchEventSources = (init = false, index = 0) => {
   return (dispatch, getState) => {
     const { eventsManager } = getState();
     const { eventSources } = eventsManager;
 
     if (index < eventSources.length) {
-      dispatch(fetchEvent(eventSources[index]));
+      dispatch(fetchEvent(eventSources[index], init, index === 0));
 
-      return dispatch(fetchEventSources(index + 1));
+      return dispatch(fetchEventSources(init, index + 1));
     } else {
       return Promise.resolve();
     }
