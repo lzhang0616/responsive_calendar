b06f66399eb23d71d2b08b244de7f747c291f981
Li Zhang <ycy828f@gmail.com>
2016-06-26 01:03:09 -0700

    lz - Hook page.js callback with redux action dispatch

--- src/actions/calendar_controls_actions.js
+++ src/actions/calendar_controls_actions.js
@@ -1,4 +1,4 @@
-import { BACK_IN_DATE, BACK_TO_TODAY, FORWARD_IN_DATE } from './actions_types';
+import { BACK_IN_DATE, BACK_TO_TODAY, FORWARD_IN_DATE, UPDATE_DATE } from './actions_types';
 
 export const backInDate = view => {
   return {
@@ -18,5 +18,12 @@ export const forwardInDate = view => {
   return {
     type: FORWARD_IN_DATE,
     view
+  };
+};
+
+export const updateDate = newDate => {
+  return {
+    type: UPDATE_DATE,
+    newDate
   };
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import 'babel-polyfill';
 import page from 'page';
+import moment from 'moment';
 import React, { Component } from 'react';
 import CalendarContainer from './containers/calendar_container';
 import thunkMiddleware from 'redux-thunk';
 import calendarApp from './reducers/calendar_app';
 import defaultInitialState from './store/default_initial_state';
-import { getRange, flattenState } from './utilities/calendar_helpers';
+import { flattenState } from './utilities/calendar_helpers';
 import { updateView } from './actions/view_controls_actions';
+import { updateDate } from './actions/calendar_controls_actions';
 import { updateEventSources, updateEventsMeta } from './actions/events_actions';
 import { createStore, applyMiddleware } from 'redux';
 import { fetchEventSources } from './actions/events_actions';
@@ -70,16 +72,28 @@ const customizeState = (options, store) => {
   dispatch(fetchEventSources(true));
 };
 
+const dispatchAction = (cxt, next) => {
+  const { date, view } = cxt.params;
+  const { dispatch, getState } = store;
+  const flatState = flattenState(getState());
+  const newDate = moment(date, ['YYYY-MM-DD'], true);
+
+  if (['day', 'week', 'month'].indexOf(view) < 0) throw `view type: ${view}`;
+
+  if (view !== flatState.view) dispatch(updateView(view));
+  if (newDate.isValid() && !newDate.isSame(flatState.date)) dispatch(updateDate(newDate));
+};
+
 const setupHashRoutes = store => {
   const { getState, subscribe } = store;
 
   page.base('/#');
-  page('/:view/:start/:end', () => {});
+  page('/:date/:view', dispatchAction);
+  page();
 
   subscribe(() => {
     const { view, date } = flattenState(getState());
-    const [ start, end ] = getRange(date, view).map(day => day.format('YYYY-MM-DD'));
 
-    page(`/${view}/${start}/${end}`);
+    page(`/${date.format('YYYY-MM-DD')}/${view}`);
   });
 };
