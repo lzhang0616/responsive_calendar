37e48b5363c2476ca7cdefde7a6d65aaa4aab361
Gary Thomas <gary.thomas@appfolio.com>
2016-07-08 16:39:36 -0700

    gt - Add webpack size statistics plugin

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,18 +1,20 @@
-'use strict'
+'use strict';
 
-var webpack = require('webpack');
-var config = require('./webpack.config');
+const webpack = require('webpack');
+const config = require('./webpack.config');
+const Visualizer = require('webpack-visualizer-plugin');
 
 config.devtool = 'cheap-module-eval-source-map';
 config.entry.splice(0, 0, 'webpack-hot-middleware/client');
-config.module.loaders[0].query.presets.splice(0,0, 'react-hmre');
+config.module.loaders[0].query.presets.splice(0, 0, 'react-hmre');
 config.plugins = [
+  new Visualizer(),
   new webpack.optimize.OccurrenceOrderPlugin(),
   new webpack.HotModuleReplacementPlugin(),
   new webpack.NoErrorsPlugin(),
   new webpack.DefinePlugin({
     'process.env': {
-      'NODE_ENV': JSON.stringify('development')
+      NODE_ENV: JSON.stringify('development')
     }
   }),
   new webpack.LoaderOptionsPlugin({
