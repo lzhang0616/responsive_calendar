6320add2a72cca8f3d94f61849ea9bdaaf2d49a2
Li Zhang <ycy828f@gmail.com>
2016-07-25 14:23:29 -0700

    lz - Lint fat arrow functions that only return an object

--- src/actions/calendar_meta_data_actions.js
+++ src/actions/calendar_meta_data_actions.js
@@ -1,71 +1,55 @@
 import { UPDATE_CALENDAR_META } from './actions_types';
 
-export const updateDayViewEventLimit = eventLimit => {
-  return {
-    type: UPDATE_CALENDAR_META,
-    calendarMeta: {
-      dayViewEventLimit: eventLimit
-    }
-  };
-};
+export const updateDayViewEventLimit = eventLimit => ({
+  type: UPDATE_CALENDAR_META,
+  calendarMeta: {
+    dayViewEventLimit: eventLimit
+  }
+});
 
-export const updateWeekViewEventLimit = eventLimit => {
-  return {
-    type: UPDATE_CALENDAR_META,
-    calendarMeta: {
-      weekViewEventLimit: eventLimit
-    }
-  };
-};
+export const updateWeekViewEventLimit = eventLimit => ({
+  type: UPDATE_CALENDAR_META,
+  calendarMeta: {
+    weekViewEventLimit: eventLimit
+  }
+});
 
-export const updateMonthViewEventLimit = eventLimit => {
-  return {
-    type: UPDATE_CALENDAR_META,
-    calendarMeta: {
-      monthViewEventLimit: eventLimit
-    }
-  };
-};
+export const updateMonthViewEventLimit = eventLimit => ({
+  type: UPDATE_CALENDAR_META,
+  calendarMeta: {
+    monthViewEventLimit: eventLimit
+  }
+});
 
-export const updateDayViewEventLimitSize = size => {
-  return {
-    type: UPDATE_CALENDAR_META,
-    calendarMeta: {
-      dayViewEventLimitSize: size
-    }
-  };
-};
+export const updateDayViewEventLimitSize = size => ({
+  type: UPDATE_CALENDAR_META,
+  calendarMeta: {
+    dayViewEventLimitSize: size
+  }
+});
 
-export const updateWeekViewEventLimitSize = size => {
-  return {
-    type: UPDATE_CALENDAR_META,
-    calendarMeta: {
-      weekViewEventLimitSize: size
-    }
-  };
-};
+export const updateWeekViewEventLimitSize = size => ({
+  type: UPDATE_CALENDAR_META,
+  calendarMeta: {
+    weekViewEventLimitSize: size
+  }
+});
 
-export const updateMonthViewEventLimitSize = size => {
-  return {
-    type: UPDATE_CALENDAR_META,
-    calendarMeta: {
-      monthViewEventLimitSize: size
-    }
-  };
-};
+export const updateMonthViewEventLimitSize = size => ({
+  type: UPDATE_CALENDAR_META,
+  calendarMeta: {
+    monthViewEventLimitSize: size
+  }
+});
 
-export const updateEventLimitClick = target => {
-  return {
-    type: UPDATE_CALENDAR_META,
-    calendarMeta: {
-      eventLimitClick: target
-    }
-  };
-};
+export const updateEventLimitClick = target => ({
+  type: UPDATE_CALENDAR_META,
+  calendarMeta: {
+    eventLimitClick: target
+  }
+});
 
-export const updateCalendarMeta = calendarMeta => {
-  return {
-    type: UPDATE_CALENDAR_META,
-    calendarMeta
-  };
-};
+export const updateCalendarMeta = calendarMeta => ({
+  type: UPDATE_CALENDAR_META,
+  calendarMeta
+});
--- src/actions/view_controls_actions.js
+++ src/actions/view_controls_actions.js
@@ -1,27 +1,19 @@
 import { SHOW_DAY_VIEW, SHOW_WEEK_VIEW,
          SHOW_MONTH_VIEW, UPDATE_VIEW } from './actions_types';
 
-export const showDayView = () => {
-  return {
-    type: SHOW_DAY_VIEW
-  };
-};
+export const showDayView = () => ({
+  type: SHOW_DAY_VIEW
+});
 
-export const showWeekView = () => {
-  return {
-    type: SHOW_WEEK_VIEW
-  };
-};
+export const showWeekView = () => ({
+  type: SHOW_WEEK_VIEW
+});
 
-export const showMonthView = () => {
-  return {
-    type: SHOW_MONTH_VIEW
-  };
-};
+export const showMonthView = () => ({
+  type: SHOW_MONTH_VIEW
+});
 
-export const updateView = view => {
-  return {
-    type: UPDATE_VIEW,
-    view
-  };
-};
+export const updateView = view => ({
+  type: UPDATE_VIEW,
+  view
+});
